b686139bb16e704e776ef109ce8083f8fb7a43bb
Jack Parsons <jcksnparsons@yahoo.com>
2020-05-04 16:31:45 -0500

    books now uses model factory higher order function

--- libraryproject/libraryapp/views/books/list.py
+++ libraryproject/libraryapp/views/books/list.py
@@ -1,13 +1,13 @@
 import sqlite3
 from django.shortcuts import render
-from libraryapp.models import *
+from libraryapp.models import Book, model_factory
 from ..connection import Connection
 
 
 def book_list(request):
     if request.method == 'GET':
         with sqlite3.connect(Connection.db_path) as conn:
-            conn.row_factory = sqlite3.Row
+            conn.row_factory = model_factory(Book)
             db_cursor = conn.cursor()
 
             db_cursor.execute("""
@@ -22,20 +22,7 @@ def book_list(request):
             from libraryapp_book b
             """)
 
-            all_books = []
-            dataset = db_cursor.fetchall()
-
-            for row in dataset:
-                book = Book()
-                book.id = row['id']
-                book.title = row['title']
-                book.isbn = row['ISBN_num']
-                book.author = row['author']
-                book.year_published = row['year_published']
-                book.librarian_id = row['librarian_id']
-                book.location_id = row['location_id']
-
-                all_books.append(book)
+            all_books = db_cursor.fetchall()
 
         template = 'books/list.html'
         context = {
