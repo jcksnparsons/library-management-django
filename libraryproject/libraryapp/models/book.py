0fec0be805559131f023dcc8a272b6b6e845e450
Jack Parsons <jcksnparsons@yahoo.com>
2020-05-04 13:32:44 -0500

    added librarian model, migrated data, created a superuser account

--- libraryproject/libraryapp/models/book.py
+++ libraryproject/libraryapp/models/book.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.urls import reverse
+from .library import Library
+from .librarian import Librarian
 
 
 class Book (models.Model):
     title = models.CharField(max_length=50)
     ISBN_num = models.CharField(max_length=13)
     author = models.CharField(max_length=50)
-    year_published = models.IntegerField(max_length=4)
-
+    year_published = models.IntegerField()
+    publisher = models.CharField(max_length=50)
+    location = models.ForeignKey(Library, on_delete=models.CASCADE)
+    librarian = models.ForeignKey(Librarian, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = ("book")
