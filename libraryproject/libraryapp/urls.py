109e4cb886d571501f3d4c749d3ccac6ce928f41
Jack Parsons <jcksnparsons@yahoo.com>
2020-05-05 09:54:56 -0500

    post functionality works

--- libraryproject/libraryapp/urls.py
+++ libraryproject/libraryapp/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('libraries/', list_libraries, name='libraries'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
+    path('book/form', book_form, name='book_form'),
 ]
--- libraryproject/libraryapp/views/books/list.py
+++ libraryproject/libraryapp/views/books/list.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from libraryapp.models import Book, model_factory
 from ..connection import Connection
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.shortcuts import redirect
 
 @login_required
 def book_list(request):
@@ -31,3 +33,23 @@ def book_list(request):
         }
 
         return render(request, template, context)
+    
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            INSERT INTO libraryapp_book
+            (
+                title, author, ISBN_num,
+                year_published, location_id, librarian_id
+            )
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            (form_data['title'], form_data['author'],
+                form_data['isbn'], form_data['year_published'],
+                request.user.librarian.id, form_data["location"]))
+
+        return redirect(reverse('libraryapp:books'))
